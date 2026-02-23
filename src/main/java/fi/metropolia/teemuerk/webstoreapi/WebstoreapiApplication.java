9a5b59ba8df9bbb67f756774a2c7ab97393b2a80
Teemu Erkkilä <teemu.erkkila@tuni.fi>
2026-02-23 17:38:38 +0200

    Adds springboot project base

---
+++ src/main/java/fi/metropolia/teemuerk/webstoreapi/WebstoreapiApplication.java
@@ -0,0 +1,13 @@
+package fi.metropolia.teemuerk.webstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebstoreapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebstoreapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/metropolia/teemuerk/webstoreapi/WebstoreapiApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.metropolia.teemuerk.webstoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebstoreapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
